1072eeb1c822c2e50d0cba908bd8f613d8fac38a
Oleg <28oleg28@mail.ru>
2019-10-03 02:52:15 +0600

    Added Xamarin Android App

---
+++ Module01/XamarinAndroidApp/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace XamarinAndroidApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button btn = FindViewById<Button>(Resource.Id.button1);
+            btn.Click += Btn_Click;
+            EditText et = FindViewById<EditText>(Resource.Id.editText1);
+        }
+
+        private void Btn_Click(object sender, System.EventArgs e)
+        {
+            EditText et = FindViewById<EditText>(Resource.Id.editText1);
+            TextView tv = FindViewById<TextView>(Resource.Id.textView1);
+            tv.Text = "Hello " + et.Text;
+        }
+    }
+}
