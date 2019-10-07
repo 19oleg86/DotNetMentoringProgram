f1fea71986b1a1c6c668c8a2f469f8421fe536c8
Oleg <28oleg28@mail.ru>
2019-10-08 01:09:10 +0600

    Added WinForms Project

---
+++ Module02/WinFormsApp/FileSystemVisitor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class FileSystemVisitor : Form
+    {
+        DirectoryInfo directoryInfo;
+        string iteratedPath;
+        public FileSystemVisitor(string path)
+        {
+            InitializeComponent();
+            directoryInfo = new DirectoryInfo(path);
+            iteratedPath = path;
+        }
+
+        public void FolderSearch()
+        {
+            int iterator = 0;
+            do
+            {
+                treeListBox.Items.AddRange(directoryInfo.GetDirectories());
+                treeListBox.Items.AddRange(directoryInfo.GetFiles());
+                directoryInfo = DirectoryIteration(treeListBox.Items[iterator].ToString());
+                iterator++;
+            }
+            while (treeListBox.Items[iterator] != string.Empty);
+        }
+
+        public DirectoryInfo DirectoryIteration(string str)
+        {
+            iteratedPath = iteratedPath + @"\" + str;
+            return new DirectoryInfo(iteratedPath);
+        }
+
+    }
+}
