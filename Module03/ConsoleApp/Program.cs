c9ebc9b2d8db2ff8979f0437306f575ebaf447df
Oleg <Oleg_Shulga@epam.com>
2019-10-14 14:13:24 +0600

    String Checking Implemented

---
+++ Module03/ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseString = null;
+            do
+            {
+                try
+                {
+                    Console.Write("Please enter a string of text: ");
+                    baseString = Console.ReadLine();
+                    CheckString cs = new CheckString();
+                    cs.CheckUserString(baseString);
+                    Console.WriteLine(baseString[0]);
+                }
+                catch (CheckStringException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            } while (baseString == string.Empty);
+        }
+    }
+
+    public class CheckString
+    {
+        public void CheckUserString(string checkThis)
+        {
+            if (checkThis.Length == 0)
+            {
+                throw new CheckStringException("The string shouldn't be empty");
+            }
+        }
+    }
+
+    public class CheckStringException : Exception
+    {
+        public CheckStringException(string message) : base(message)
+        { }
+    }
+
+}
