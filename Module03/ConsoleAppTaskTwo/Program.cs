2bf130f72576253a7bf907d0ea16edb0aae816e5
Oleg <Oleg_Shulga@epam.com>
2019-10-15 10:38:05 +0600

    Added second task

---
+++ Module03/ConsoleAppTaskTwo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using StringTransformer;
+
+namespace ConsoleAppTaskTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseString;
+            IntMaker intMaker = new IntMaker();
+            do
+            {
+                Console.Write("Enter a number (or enter \"exit\" to finish the program): ");
+                baseString = Console.ReadLine();
+                if (baseString == "exit")
+                    break;
+                    try
+                    {
+                        Console.WriteLine($"Your number is: {intMaker.CovertToInt(baseString)}");
+                    }
+                    catch (EmptyStringExc ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                    catch (IncorrectFormatExc ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                    catch (TypeOverFlowExc ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine($"Error: Extremely large number, enter a lower number (between -2 147 483 648 and  2 147 483 647)");
+                    }
+            } while (1 == 1);
+        }
+    }
+}
---
+++ Module03/StringTransformer/IntMaker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StringTransformer
+{
+    public class IntMaker
+    {
+        public long CovertToInt(string sourceString)
+        {
+            if (sourceString.Length == 0)
+            {
+                throw new EmptyStringExc("The Input mustn't be empty");
+            }
+            for (int i = 0; i < sourceString.Length; i++)
+            {
+                if (sourceString[i] >= '0' && sourceString[i] <= '9')
+                    continue;
+                else
+                {
+                    throw new IncorrectFormatExc("The Input must contain only digits from 0 to 9");
+                }
+            }
+            long interimNumber = Convert.ToInt64(sourceString);
+            if (interimNumber > 2147483647 || interimNumber < -2147483648)
+            {
+                throw new TypeOverFlowExc("Your number too big, it must be between -2 147 483 648 and  2 147 483 647");
+            }
+            return interimNumber;
+        }
+    }
+
+    public class EmptyStringExc : Exception
+    {
+        public EmptyStringExc(string message) : base(message)
+        {}
+    }
+    public class IncorrectFormatExc : Exception
+    {
+        public IncorrectFormatExc(string message) : base(message)
+        {}
+    }
+    public class TypeOverFlowExc : Exception
+    {
+        public TypeOverFlowExc(string message) : base(message) 
+        {}
+    }
+}
