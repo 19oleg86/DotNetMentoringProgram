25d2d8fbb6c8f3c22186b2dae4a6057174960001
Oleg <Oleg_Shulga@epam.com>
2019-10-17 17:31:14 +0600

    Added last fixed test

--- Module03/StringTransformer/IntMaker.cs
+++ Module03/StringTransformer/IntMaker.cs
@@ -21,21 +21,25 @@ namespace StringTransformer
             }
             int interimNumber = 0;
             char[] array = sourceString.ToCharArray();
-
+            var negative = sourceString.Contains("-");
             for (int i = 0; i < array.Length; i++)
-                
-                    if (array[array.Length - i - 1] == '-')
+                checked
+                {
+                    var currentChar = array[array.Length - i - 1];
+                    if (currentChar == '-')
                     {
-                        interimNumber *= -1;
+                        continue;
+                    }
+                    var diff = (int)((currentChar - '0') * Math.Pow(10, i));
+                    if (negative)
+                    {
+                        interimNumber -= diff;
                     }
                     else
                     {
-                    checked
-                    {
-                        interimNumber += ((int)((array[array.Length - i - 1] - '0') * Math.Pow(10, i)));
+                        interimNumber += diff;
                     }
                 }
-                
 
             if (interimNumber > int.MaxValue || interimNumber < int.MinValue)
             {
