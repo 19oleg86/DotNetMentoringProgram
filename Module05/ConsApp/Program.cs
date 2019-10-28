db8e240a775c0cbf649811f91ed7a48b55694557
Oleg <28oleg28@mail.ru>
2019-10-29 00:52:12 +0600

    Started doing Module05

---
+++ Module05/ConsApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Ctrl + C for exit");
+            while (true)
+            {
+                var k = Console.ReadKey(true);
+                if ((k.Modifiers & ConsoleModifiers.Control) != 0)
+                {
+                    if ((k.Key & ConsoleKey.C) != 0)
+                    {
+                        break;
+                    }
+                }
+
+                string[] pathesToWatch = Environment.GetCommandLineArgs();
+                
+                using (FileSystemWatcher watcher = new FileSystemWatcher())
+                {
+
+                }
+            } 
+        }
+    }
+}
