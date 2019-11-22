889304216c130a72fb185a9bc097fb47c85284eb
Oleg <Oleg_Shulga@epam.com>
2019-11-22 10:01:22 +0600

    finished Module08

---
+++ Module08/Task3/3.2 (SSDT)/Northwind/Northwind_v.1.1/dbo/Tables/EmployeesCreditCardData.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE "EmployeesCreditCardData" (
+	"CardNumber" "int" NOT NULL ,
+	"ExpirationDate" "datetime" NOT NULL ,
+	"CardHolderName" nvarchar (30) NOT NULL ,
+	"LinkToEmployee" nvarchar (255) NULL ,
+)
