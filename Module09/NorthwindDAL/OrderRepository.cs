585fd9e0bc46281082de0220ad2fb3b5e18dc62b
Oleg <Oleg_Shulga@epam.com>
2019-11-26 16:54:28 +0600

    Started doing Module09

---
+++ Module09/NorthwindDAL/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+
+namespace NorthwindDAL
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DbProviderFactory providerFactory;
+        private readonly string connectionString;
+        public IEnumerable<Order> GetOrders()
+        {
+            using (var connection = providerFactory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+            }
+        }
+    }
+}
