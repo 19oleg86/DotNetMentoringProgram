394e09c123ae1577cc956faa62e3f3ba34928dd4
Oleg <Oleg_Shulga@epam.com>
2019-11-28 17:14:06 +0600

    added test project

---
+++ Module09/NorthwindDALTests/OrderRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NorthwindDAL;
+
+namespace NorthwindDALTests
+{
+    [TestClass]
+    public class OrderRepositoryTests
+    {
+        [TestMethod]
+        public void GetOrders_IEnumerableOfOrderReturned()
+        {
+            //Arrange
+            var mock = new Mock<IOrderRepository>();
+            mock.Setup(a => a.GetOrders()).Returns(new List<Order>());
+            IOrderRepository orderRepository = new OrderRepository(mock.Object);
+
+            //Act
+            List<Order> result = orderRepository.GetOrders() as List<Order>;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.
+        }
+    }
+}
