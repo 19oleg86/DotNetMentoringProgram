151edbcea48cce671e9c70a040c31549dedd5967
Oleg <Oleg_Shulga@epam.com>
2019-12-04 20:15:32 +0600

    second migration

---
+++ Module10/Task2EntityFramework_Versions/EmployeeCreditCardData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2EntityFramework_Versions
+{
+    class EmployeeCreditCardData
+    {
+        public string CardNumber { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public string CardHolderName { get; set; }
+
+        public string LinkToEmployee { get; set; }
+    }
+}
---
+++ Module10/Task2EntityFramework_Versions/Migrations/201912041412386_Version_2.0.cs
@@ -0,0 +1,16 @@
+namespace Task2EntityFramework_Versions.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Version_20 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
