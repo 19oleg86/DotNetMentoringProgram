ba2c1ac52989735238f7ed7256d065079ed4b808
Oleg <Oleg_Shulga@epam.com>
2019-12-09 17:05:59 +0600

    finished 11 Module

--- Module11/MongoDbApp/Program.cs
+++ Module11/MongoDbApp/Program.cs
@@ -11,172 +11,57 @@ namespace MongoDbApp
 {
     class Program
     {
-        
         public static void Main(string[] args)
         {
             string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            MongoClient client = new MongoClient(connectionString);
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("library");
+            var collection = database.GetCollection<BsonDocument>("books");
 
-            Console.WriteLine("Adding data to Database...");
-            SaveDocs().GetAwaiter().GetResult();
+            Console.WriteLine("Task 1 - Adding data to Database...");
+            SaveDocs(collection).GetAwaiter().GetResult();
             Console.WriteLine("Data added to Database");
-
             Console.WriteLine();
 
             Console.WriteLine("Display all documents: ");
-            DisplayDocs().GetAwaiter().GetResult();
+            DisplayDocs(collection).GetAwaiter().GetResult();
             Console.WriteLine();
 
-            Console.WriteLine("Books with count more than 1:");
-            FindBooksMoreThanOne().GetAwaiter().GetResult();
+            Console.WriteLine("Task 2 - Books with count more than 1:");
+            FindBooksMoreThanOne(collection).GetAwaiter().GetResult();
             Console.WriteLine();
 
-            Console.WriteLine("Books with Maximum and Minimum Count values:");
-            FindBooksWithMaxAndMinCount().GetAwaiter().GetResult();
+            Console.WriteLine("Task 3 - Books with Maximum and Minimum Count values:");
+            FindBooksWithMaxAndMinCount(collection).GetAwaiter().GetResult();
             Console.WriteLine();
 
-            Console.WriteLine("All authors (without duplicates):");
-            FindAllAuthorsOnce().GetAwaiter().GetResult();
+            Console.WriteLine("Task 4 - All authors (without duplicates):");
+            FindAllAuthorsOnce(collection).GetAwaiter().GetResult();
             Console.WriteLine();
 
-            Console.WriteLine("All books without authors:");
-            FindAllBooksWithoutAuthors().GetAwaiter().GetResult();
+            Console.WriteLine("Task 5 - All books without authors:");
+            FindAllBooksWithoutAuthors(collection).GetAwaiter().GetResult();
             Console.WriteLine();
 
-            Console.WriteLine("Change each book count on 1:");
-            ChangeEachBookCountOnOne().GetAwaiter().GetResult();
+            Console.WriteLine("Task 6 - Change each book count on 1:");
+            ChangeEachBookCountOnOne(collection).GetAwaiter().GetResult();
             Console.WriteLine();
 
-            Console.WriteLine("Add additional genre Favority where Fantasy exists");
-            AddAdditionalGenreWhereFantasy().GetAwaiter().GetResult();
+            Console.WriteLine("Task 7 - Add additional genre Favority where Fantasy exists");
+            AddAdditionalGenreWhereFantasy(collection).GetAwaiter().GetResult();
             Console.WriteLine();
 
-            Console.WriteLine("Press Enter to clear the database");
+            Console.WriteLine("Task 8 - Delete books where count is less than 3");
+            DeleteBooksWhereCountLessThanThree(collection).GetAwaiter().GetResult();
+            Console.WriteLine();
+
+            Console.WriteLine("Press Enter to clear the database (Task 9 - Delete all documents)");
             Console.ReadLine();
-            DeleteBooks().GetAwaiter().GetResult();
+            DeleteBooks(collection).GetAwaiter().GetResult();
         }
 
-        private static async Task AddAdditionalGenreWhereFantasy()
+        private static async Task SaveDocs(IMongoCollection<BsonDocument> collection)
         {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("library");
-            var collection = database.GetCollection<BsonDocument>("books");
-            var filter = Builders<BsonDocument>.Filter.Eq("Genre", "Fantasy");
-            var update = Builders<BsonDocument>.Update.AddToSet("Genre", "Favority");
-            var books = await collection.UpdateManyAsync(filter, update);
-            Console.WriteLine("Display all documents:");
-            DisplayDocs().GetAwaiter().GetResult();
-            Console.WriteLine();
-        }
-
-        private static async Task ChangeEachBookCountOnOne()
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("library");
-            var collection = database.GetCollection<BsonDocument>("books");
-            var books = await collection.UpdateManyAsync(new BsonDocument(), new BsonDocument("$inc", new BsonDocument("Count", 1)));
-            Console.WriteLine("Display all documents:");
-            DisplayDocs().GetAwaiter().GetResult();
-            Console.WriteLine();
-        }
-
-        private static async Task FindAllBooksWithoutAuthors()
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("library");
-            var collection = database.GetCollection<BsonDocument>("books");
-            var filter = new BsonDocument("Author", "");
-            var books = await collection.Find(filter).Project("{_id:0}").ToListAsync();
-            var fiteredBooks = books.Distinct();
-            foreach (var doc in fiteredBooks)
-            {
-                Console.WriteLine(doc);
-            }
-        }
-
-        private static async Task FindAllAuthorsOnce()
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("library");
-            var collection = database.GetCollection<BsonDocument>("books");
-            var filter = new BsonDocument();
-            var books = await collection.Find(filter).Project("{Author:1, _id:0}").ToListAsync();
-            var fiteredBooks = books.Distinct();
-            foreach (var doc in fiteredBooks)
-            {
-                Console.WriteLine(doc);
-            }
-        }
-
-        private static async Task FindBooksWithMaxAndMinCount()
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("library");
-            var collection = database.GetCollection<BsonDocument>("books");
-            var filter = new BsonDocument();
-            var books = await collection.Find(filter).Sort("{Count:-1}").Limit(1).Project("{_id:0}").ToListAsync();
-            foreach (var doc in books)
-            {
-                Console.WriteLine("Book with Maximum of Count: " + doc);
-            }
-            books = await collection.Find(filter).Sort("{Count:1}").Limit(1).Project("{_id:0}").ToListAsync();
-            foreach (var doc in books)
-            {
-                Console.WriteLine("Book with Minimum of Count: " + doc);
-            }
-        }
-
-        private static async Task FindBooksMoreThanOne()
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("library");
-            var collection = database.GetCollection<BsonDocument>("books");
-            var filter = new BsonDocument("Count", new BsonDocument("$gt", 1));
-            var books = await collection.Find(filter).Sort("{Name:1}").Limit(3).Project("{Name:1, _id:0}").ToListAsync();
-            foreach (var doc in books)
-            {
-                Console.WriteLine(doc);
-            }
-            Console.WriteLine($"Number of books: {books.Count}"); 
-        }
-
-        private static async Task DeleteBooks()
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("library");
-            var collection = database.GetCollection<BsonDocument>("books");
-
-            var filter = new BsonDocument();
-            await collection.DeleteManyAsync(filter);
-        }
-
-        private static async Task DisplayDocs()
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("library");
-            var collection = database.GetCollection<BsonDocument>("books");
-            var filter = new BsonDocument();
-            var books = await collection.Find(filter).ToListAsync();
-            foreach (var doc in books)
-            {
-                Console.WriteLine(doc);
-            }
-        }
-
-        private static async Task SaveDocs()
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase("library");
-            var collection = database.GetCollection<BsonDocument>("books");
             BsonDocument book1 = new BsonDocument
             {
                 {"Name", "Hobbit"},
@@ -218,6 +103,97 @@ namespace MongoDbApp
                 {"Year", 2001}
             };
             await collection.InsertManyAsync(new[] { book1, book2, book3, book4, book5 });
+        }
+
+        private static async Task DisplayDocs(IMongoCollection<BsonDocument> collection)
+        {
+            var filter = new BsonDocument();
+            var books = await collection.Find(filter).ToListAsync();
+            foreach (var doc in books)
+            {
+                Console.WriteLine(doc);
+            }
+        }
+
+        private static async Task FindBooksMoreThanOne(IMongoCollection<BsonDocument> collection)
+        {
+            var filter = new BsonDocument("Count", new BsonDocument("$gt", 1));
+            var books = await collection.Find(filter).Sort("{Name:1}").Limit(3).Project("{Name:1, _id:0}").ToListAsync();
+            foreach (var doc in books)
+            {
+                Console.WriteLine(doc);
+            }
+            Console.WriteLine($"Number of books: {books.Count}");
+        }
+
+        private static async Task FindBooksWithMaxAndMinCount(IMongoCollection<BsonDocument> collection)
+        {
+            var filter = new BsonDocument();
+            var books = await collection.Find(filter).Sort("{Count:-1}").Limit(1).Project("{_id:0}").ToListAsync();
+            foreach (var doc in books)
+            {
+                Console.WriteLine("Book with Maximum of Count: " + doc);
+            }
+            books = await collection.Find(filter).Sort("{Count:1}").Limit(1).Project("{_id:0}").ToListAsync();
+            foreach (var doc in books)
+            {
+                Console.WriteLine("Book with Minimum of Count: " + doc);
+            }
+        }
+
+        private static async Task FindAllAuthorsOnce(IMongoCollection<BsonDocument> collection)
+        {
+            var filter = new BsonDocument();
+            var books = await collection.Find(filter).Project("{Author:1, _id:0}").ToListAsync();
+            var fiteredBooks = books.Distinct();
+            foreach (var doc in fiteredBooks)
+            {
+                Console.WriteLine(doc);
+            }
+        }
+
+        private static async Task FindAllBooksWithoutAuthors(IMongoCollection<BsonDocument> collection)
+        {
+            var filter = new BsonDocument("Author", "");
+            var books = await collection.Find(filter).Project("{_id:0}").ToListAsync();
+            var fiteredBooks = books.Distinct();
+            foreach (var doc in fiteredBooks)
+            {
+                Console.WriteLine(doc);
+            }
+        }
+
+        private static async Task ChangeEachBookCountOnOne(IMongoCollection<BsonDocument> collection)
+        {
+            var books = await collection.UpdateManyAsync(new BsonDocument(), new BsonDocument("$inc", new BsonDocument("Count", 1)));
+            Console.WriteLine("Display all documents:");
+            DisplayDocs(collection).GetAwaiter().GetResult();
+            Console.WriteLine();
+        }
+
+        private static async Task AddAdditionalGenreWhereFantasy(IMongoCollection<BsonDocument> collection)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("Genre", "Fantasy");
+            var update = Builders<BsonDocument>.Update.AddToSet("Genre", "Favority");
+            var books = await collection.UpdateManyAsync(filter, update);
+            Console.WriteLine("Display all documents:");
+            DisplayDocs(collection).GetAwaiter().GetResult();
+            Console.WriteLine();
+        }
+
+        private static async Task DeleteBooksWhereCountLessThanThree(IMongoCollection<BsonDocument> collection)
+        {
+            var filter = Builders<BsonDocument>.Filter.Lt("Count", 3);
+            await collection.DeleteManyAsync(filter);
+            Console.WriteLine("Display all documents:");
+            DisplayDocs(collection).GetAwaiter().GetResult();
+            Console.WriteLine();
+        }
+
+        private static async Task DeleteBooks(IMongoCollection<BsonDocument> collection)
+        {
+            var filter = new BsonDocument();
+            await collection.DeleteManyAsync(filter);
         }
     }
 }
