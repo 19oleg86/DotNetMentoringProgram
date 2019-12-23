1eb9a429ba2f747c25630f59a8e00e38ebea78f0
Oleg <Oleg_Shulga@epam.com>
2019-12-23 16:56:06 +0600

    started second task

---
+++ Module13/SimpleHttpHandler/SimpleHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleHttpHandler
+{
+    public class SimpleHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpResponse response = context.Response;
+            response.Write("<html><body><h1>Wow.. We created our first handler");
+            response.Write("</h1></body></html>");
+        }
+    }
+}
