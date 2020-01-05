35ebc8521c7411c9938657f47e1c9cfcb24ba036
Oleg <Oleg_Shulga@epam.com>
2020-01-05 17:03:08 +0600

    added module 15

---
+++ Module15/Task1FibonacciCache/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1FibonacciCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+        
+    }
+
+    class Fibonacci
+    {
+        public List<int> listOfNumbers = new List<int>();
+
+        public int GetCachedFibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return GetCachedFibonacci(n - 1) + GetCachedFibonacci(n - 2);
+            }
+            
+            //.Add(firstNumber);
+
+
+        }
+    }
+}
