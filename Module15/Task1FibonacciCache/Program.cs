74417e1606a50192b66edb3f37982e96c6867a3e
Oleg <Oleg_Shulga@epam.com>
2020-01-09 16:51:35 +0600

    Made some changes

--- Module15/Task1FibonacciCache/Program.cs
+++ Module15/Task1FibonacciCache/Program.cs
@@ -12,32 +12,11 @@ namespace Task1FibonacciCache
         static void Main(string[] args)
         {
             // Fibonacci row - 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
-            List<int> finalList;
-            List<int> tryToFind = new List<int>() { 3, 8, 5 };
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-           
             // Trying to get 3 then 8 and then 5 from Fibonacci row
-            foreach (var item in tryToFind)
-            {
-                foreach (var digit in Fibonacci.cache)
-                {
 
-                    if (dict.ContainsValue(item))
-                    {
-                        Console.WriteLine((int)Fibonacci.cache.Get(item.ToString()) + " - from cache");
-                        break;
-                    }
-                }
-                finalList = Fibonacci.GetCachedFibonacci(item);
-                foreach (var number in finalList)
-                {
-                    if (dict.Keys.Contains(number.ToString()))
-                        continue;
-                    dict.Add(number.ToString(), number);
-                    Console.WriteLine(number + " - not from cache");
-                }
-                Console.WriteLine();
-            }
+            List<int> tryToFind = new List<int>() { 3, 8, 5 };
+            Fibonacci.GetCachedFibonacci(tryToFind);
+
             Console.ReadLine();
         }
     }
@@ -49,25 +28,50 @@ namespace Task1FibonacciCache
         public static int next;
         public static List<int> listOfNumbers = new List<int>();
         public static Cache cache = new Cache();
-        public static List<int> GetCachedFibonacci(int n)
+        public static Dictionary<int, int> dict = new Dictionary<int, int>();
+        public static void GetCachedFibonacci(List<int> searchNumbers)
         {
-            if (first == second)
+            cache.Insert("CN", dict);
+            foreach (var item in searchNumbers)
             {
-                listOfNumbers.Add(first);
-                listOfNumbers.Add(second);
-                cache.Insert(first.ToString(), first);
-                cache.Insert(second.ToString(), second);
-            }
-            while (!listOfNumbers.Contains(n))
-            {
-                next = first + second;
-                listOfNumbers.Add(next);
-                cache.Insert(next.ToString(), next);
-                first = second;
-                second = next;
-            }
+                if (first == second)
+                {
+                    listOfNumbers.Add(first);
+                    listOfNumbers.Add(second);
+                    dict.Add(first, first);
+                    if (!dict.Keys.Contains(second))
+                    dict.Add(second, second);
+                }
+                while (!listOfNumbers.Contains(item))
+                {
+                    next = first + second;
+                    listOfNumbers.Add(next);
+                    dict.Add(next, next);
+                    first = second;
+                    second = next;
+                }
 
-            return listOfNumbers;
+                foreach (var digit in dict.Values)
+                {
+                    if (digit == item)
+                    {
+                        var innerDict = (Dictionary<int, int>)cache.Get("CN");
+                        int result;
+                        innerDict.TryGetValue(item, out result);
+                        Console.WriteLine( result + " - from cache");
+                        break;
+                    }
+                }
+
+                foreach (var number in listOfNumbers)
+                {
+                    //if (dict.Keys.Contains(number.ToString()))
+                    //    continue;
+                    //dict.Add(number.ToString(), number);
+                    Console.WriteLine(number + " - not from cache");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
